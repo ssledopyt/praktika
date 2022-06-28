bd58102354976ef2d54886cef7f574b7b6d2343c
Andrei <turutin2002@gmail.com>
2022-06-28 17:01:34 +0300

    ustal
    ещё пару методов добавили

--- lesson 3/orm.py
+++ lesson 3/orm.py
@@ -19,14 +19,52 @@ class Database:
         except:
             pass
 
+    def try_smth(self, table):
+        self.cursor.execute(f"""
+            PRAGMA table_info({table.name});""")
+        return self.cursor.fetchall()
+
     def try_add_columns(self, table, **kwargs):
         try:
-            self.cursor.execute(f"""
-                ALERT TABLE {table} ADD COLUMN(
-                name NOT NULL KEY);
-                """)
+            for name_of_column, type_of_column in kwargs.items():
+                self.cursor.execute(f"""
+                    ALTER TABLE {table.name} ADD COLUMN
+                    {name_of_column} {type_of_column};
+                    """)
+                self.connection.commit()
         except:
             pass
+
+    def get_table_object(self, table, name_of_column, value_of_column):
+        self.cursor.execute(f"""SELECT * FROM {table.name}
+        WHERE {name_of_column} = {value_of_column};
+        """)
+        return self.cursor.fetchall()
+
+    def create_table_object(self, table, *args):
+        param = ""
+        for i in args:
+            if isinstance(i, str):
+                param += "'" + str(i) + "'" + ','
+            else:
+                param += str(i) + ','
+        param = param[:-1]
+        self.cursor.execute(f"""
+        INSERT INTO {table.name}({','.join(table.colums.keys())})
+        VALUES ({param});
+        """)
+        self.connection.commit()
+        return self.cursor.fetchall()
+
+    def update_table_object(self, table, name_of_column, value_of_column, *args):
+        for index, column in enumerate(list(table.colums.keys())[1:]):
+            self.cursor.execute(f"""
+            UPDATE {table.name} SET {column} = {args[index] if isinstance(args[index], int) 
+            else "'" + args[index] +"'"} where {name_of_column} = {value_of_column};
+            """)
+            self.connection.commit()
+
+    def delete_table_object(self,):
 
     def __delete__(self, instance):
         self.cursor.close()
@@ -35,13 +73,29 @@ class Database:
 
 class Table:
     def __init__(self, name, datab):
+        self.colums = {}
         self.datab = datab
         self.name = name
+        self.datab.try_add_table(name)
+        for column in self.datab.try_smth(self):
+            self.colums[column[1]] = column[2]
 
-    def add_columns(self, **kwargs):
+    def add_colums(self, **kwargs):
+        for name_of_column, type_of_column in kwargs.items():
+            self.colums[name_of_column] = type_of_column
+        self.datab.try_add_columns(self, **kwargs)
         print(kwargs)
 
+    def create_objects(self, *args):
+        self.datab.create_table_object(self, *args)
+
+    def try_smth(self):
+        self.datab.try_smth(self)
 
 db = Database('test')
 anims = Table('anims', db)
-anims.add_columns()
+columns = {'name': 'TEXT'}
+anims.add_colums(**columns)
+anims.try_smth()
+anims.create_objects(1, 'alica')
+print(anims.colums)
