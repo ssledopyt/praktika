209fa0c40e443dd5140852605773c71aabef9529
Andrei <turutin2002@gmail.com>
2022-06-28 15:05:54 +0300

    Создали db

--- lesson 3/main.py
+++ lesson 3/main.py
@@ -1,19 +1,19 @@
 import sqlite3
 
 try:
-    sqlite_connection = sqlite3.connect('sqlite_python.db')
+    sqlite_connection = sqlite3.connect('test.db')
     cursor = sqlite_connection.cursor()
-    print("База данных создана и успешно подключена к SQLite")
+    print("База данных создана и успешно подключена к sqlite")
 
-    sqlite_select_query = "select sqlite_version();"
+    sqlite_select_query = "SELECT sqlite_version();"
     cursor.execute(sqlite_select_query)
     record = cursor.fetchall()
-    print("Версия базы данных SQLite: ", record)
+    print("версия базы данных sqlite: ", record)
     cursor.close()
 
 except sqlite3.Error as error:
-    print("Ошибка при подключении к sqlite", error)
+    print("ошибка при подключении к sqlite", error)
 finally:
     if (sqlite_connection):
         sqlite_connection.close()
-        print("Соединение с SQLite закрыто")
+        print("соединение с sqlite закрыто")
