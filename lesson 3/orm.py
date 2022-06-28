dd46c5d929b2b7159c55180f509063fea67f8723
Andrei <turutin2002@gmail.com>
2022-06-28 15:57:57 +0300

    Делаем методы

--- lesson 3/orm.py
+++ lesson 3/orm.py
@@ -4,14 +4,44 @@ import sqlite3
 class Database:
     def __init__(self, name):
         self.connection = sqlite3.connect(f'{name}.db')
+        self.cursor = self.connection.cursor()
+
 
     def inspect(self):
         print(self.__dict__)
 
+    def try_add_table(self, name):
+        try:
+            self.cursor.execute(f"""
+                create table {name} (
+                id INTEGER PRIMARY KEY);
+                """)
+        except:
+            pass
+
+    def try_add_columns(self, table, **kwargs):
+        try:
+            self.cursor.execute(f"""
+                ALERT TABLE {table} ADD COLUMN(
+                name NOT NULL KEY);
+                """)
+        except:
+            pass
+
     def __delete__(self, instance):
+        self.cursor.close()
         self.connection.close()
         super().__delete__(self, instance)
 
-        
+class Table:
+    def __init__(self, name, datab):
+        self.datab = datab
+        self.name = name
+
+    def add_columns(self, **kwargs):
+        print(kwargs)
+
+
 db = Database('test')
-db.inspect()
+anims = Table('anims', db)
+anims.add_columns()
